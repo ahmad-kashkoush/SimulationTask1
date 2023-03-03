d115a3ca9b9484b03d85effa7f953e82762a8f33
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-03-03 14:07:58 +0200

    Initial structure

---
+++ Task1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void TakeInput(){}
+void DoProcess(){}
+void PrintOutput(){}
+int main(){
+    TakeInput();
+    DoProcess();
+    PrintOutput();
+    return 0;
+}
